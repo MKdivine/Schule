dac8044454a6db8760cd1c4422334d9238102ad2
Hannahsump <hansgerk2003@gmail.com>
2024-08-17 23:30:33 +0200

    Seperated returnstring into a function; better design

--- 3.py
+++ 3.py
@@ -3,12 +3,12 @@ def primzahlen(y):
     i = 2
     x = 2
     while len(prime) < y:
-        while i % x != 0 and i != x :
-            x += 1  
-        if i == x: 
-            prime.append(i)   
-        i += 1    
-        x = 2 
+        while i % x != 0 and i != x:
+            x += 1
+        if i == x:
+            prime.append(i)
+        i += 1
+        x = 2
     return prime
 
 #print(primzahlen(1000))
@@ -20,16 +20,33 @@ def primfaktorzerlegung(x):
     prim = primzahlen(LIMIT)
 
 
-    Exit = ""
+    ausgabe = []
     while x != 1:
         while x % prim[i] != 0:
             if i == LIMIT-1:
-                return "Zahl zu groß"
+                return 0
             else:
                 i += 1
-        x = x / prim[i]  
-        Exit += str(prim[i]) + " "
+        x = x / prim[i]
+        ausgabe.append(prim[i]) 
         i = 0
-    return Exit
+    return ausgabe
 
-print (primfaktorzerlegung(100000))
+
+def Output_function(number):
+    Output = primfaktorzerlegung(number)
+    Awnser = "Die Primfaktorzerlegung von " + str(number) + " = "
+    i = 0
+    if Output == 0:
+        return "Die Zahl ist zu groß"
+    else:
+        print(Output)
+        while i < len(Output)-1:
+            Awnser += str(Output[i]) + "x"
+            i += 1
+        Awnser += str(Output[i])
+        return Awnser
+
+
+User_Input = 600851475143
+print (Output_function(User_Input))
