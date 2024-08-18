965155345f4da517809091252d5a2a7539f27f12
MKdivine <jensboerger23@gmail.com>
2024-08-18 03:54:10 +0200

    added two other solutions

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+#original by kuzze
+
 def mathfunction(x):
     i=0
     value = 0
@@ -8,3 +10,19 @@ def mathfunction(x):
     return value
 
 print(mathfunction(1000))
+
+#added another solution using a for loop
+def mathloop(num):
+    sum = 0
+    for x in range(0, num):
+        if x % 3 == 0 or x % 5 == 0:
+            sum +=x
+    return sum
+
+print(mathloop(1000))
+
+
+#soltion as lambda
+
+lol = sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
+print(lol)
